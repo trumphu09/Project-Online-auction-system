130ed20dc80a0aa3b643eeed32f4089cf347514b
Phan Xuan Thanh <25020397@vnu.edu.vn>
2026-03-26 20:02:55 +0700

    merge

--- Server/src/main/java/com/auction/server/models/AuctionManager.java
+++ Server/src/main/java/com/auction/server/models/AuctionManager.java
@@ -1,24 +1,14 @@
 package com.auction.server.models;
-<<<<<<< HEAD
 import java.util.ArrayList;
 import java.util.List;
 
 public class AuctionManager {
 
-=======
-import java.util.List;
-import java.util.ArrayList;
-public class AuctionManager{
->>>>>>> a7c57267a2ad1bc874e3b227bfa0d30b8149305c
     private static AuctionManager instance;
     private final List<User> users;
     private final List<Auction> auctions;
 
-<<<<<<< HEAD
     private AuctionManager() {
-=======
-    private AuctionManager(){
->>>>>>> a7c57267a2ad1bc874e3b227bfa0d30b8149305c
         users = new ArrayList<>();
         auctions = new ArrayList<>();
         System.out.println(">> Hệ thống quản lý đấu giá đã khởi động!");
@@ -54,25 +44,15 @@ public class AuctionManager{
         return null; // Không tìm thấy
     }
 
-<<<<<<< HEAD
     // Controller đứng ra nhận lệnh đặt giá
     public boolean processBidRequest(String auctionId, Bidder bidder, double amount) {
         Auction auction = getAuctionById(auctionId);
         if (auction != null) {
             // Chuyển việc xử lý chi tiết cho class Auction tự lo
-=======
-    public boolean processBidRequest(String auctionId, Bidder bidder, double amount) {
-        Auction auction = getAuctionById(auctionId);
-        if (auction != null) {
->>>>>>> a7c57267a2ad1bc874e3b227bfa0d30b8149305c
             return auction.placeBid(bidder, amount);
         } else {
             System.out.println("Lỗi: Không tìm thấy phiên đấu giá " + auctionId);
             return false;
         }
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> a7c57267a2ad1bc874e3b227bfa0d30b8149305c
